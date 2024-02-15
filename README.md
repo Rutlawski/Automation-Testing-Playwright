9ddc1fdbb425d0a51c80654178d26e23a7c7fb4e
Marek Ostrowski <rutlawski2@wp.pl>
2024-02-15 14:55:07 +0100

    added playwright testshop files

--- README.md
+++
@@ -1,4 +0,0 @@
-# Automation-Testing-Playwright
-This is another repository containg some automated tests, written regarding training and various courses, this time concerning Playwright + JavaScript.
-/
-Kolejne repozytorium, zawierające zautoatyzowane testy, napisane podczas nauki oraz kursów. Tym razem w Playwright + JavaScript. 
--- tests/homePage.spec.js
+++
@@ -1,9 +0,0 @@
-import test, { expect } from "@playwright/test";
-import { Homepage } from "../pages/Homepage";
-import { titles } from "../lib/titles";
-
-test("Opens homepage and verifies title", async ({page}) => {
-    const homepage = new Homepage(page);
-    await homepage.openHomepage();
-    await expect(page).toHaveTitle(titles.homepageTitle);
-})
